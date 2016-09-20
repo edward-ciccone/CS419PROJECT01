619fb7725f6ceceb1bb920ee1f780d8f7940a4a2
edward <etpciccone@gmail.com>
2016-09-20 11:36:54 -0500

    updated RationalTest.java

--- src/RationalTest.java
+++ src/RationalTest.java
@@ -59,17 +59,12 @@ public class RationalTest {
 			opStr = lhs.toString() + " / " + rhs.toString();
 			lhs.div(rhs);
 		}
-		System.out.printf("%15s%15s%n", opStr, lhs.toString());
+		System.out.printf("%15s%15s%n%n", opStr, lhs.toString());
 	}
 
 	private static void divStaticRationals() {
 		System.out.println("\n\nDivision: \n\n");
-
 		System.out.printf("%15s%15s%n", "Operation", "Result");
-		/*
-		 * Adds every other Rational objects to one another,
-		 * and displays the sum.
-		 */
 		for(int i = 0; i < 10; i+=2) {
 			Rational a = rationals.get(i);
 			Rational b = rationals.get(i+1);
@@ -78,16 +73,12 @@ public class RationalTest {
 			String res = temp.toString();
 			System.out.printf("%15s%15s%n", op, res);
 		}
+		System.out.println();
 	}
 	
 	private static void multStaticRationals() {
 		System.out.println("\n\nMultiplication: \n\n");
-
 		System.out.printf("%15s%15s%n", "Operation", "Result");
-		/*
-		 * Adds every other Rational objects to one another,
-		 * and displays the sum.
-		 */
 		for(int i = 0; i < 10; i+=2) {
 			Rational a = rationals.get(i);
 			Rational b = rationals.get(i+1);
@@ -100,12 +91,7 @@ public class RationalTest {
 	
 	private static void subStaticRationals() {
 		System.out.println("\n\nSubtraction: \n\n");
-
 		System.out.printf("%15s%15s%n", "Operation", "Result");
-		/*
-		 * Adds every other Rational objects to one another,
-		 * and displays the sum.
-		 */
 		for(int i = 0; i < 10; i+=2) {
 			Rational a = rationals.get(i);
 			Rational b = rationals.get(i+1);
@@ -118,12 +104,7 @@ public class RationalTest {
 	
 	private static void addStaticRationals() {
 		System.out.println("\n\nAddition: \n\n");
-
 		System.out.printf("%15s%15s%n", "Operation", "Result");
-		/*
-		 * Adds every other Rational objects to one another,
-		 * and displays the sum.
-		 */
 		for(int i = 0; i < 10; i+=2) {
 			Rational a = rationals.get(i);
 			Rational b = rationals.get(i+1);
@@ -137,7 +118,6 @@ public class RationalTest {
 	private static void displayRationals() {
 		System.out.println("The Values: \n\n");
 		System.out.printf("%15s%15s%15s%n", "Numerator", "Denominator", "Rational");
-
 		/* Creates 10 random Rational objects using the randomly generated numbers */
 		for(int i = 0; i < ARR_SIZE; i++) {
 			rationals.add(new Rational(numerators.get(i), denominators.get(i)));
@@ -149,7 +129,6 @@ public class RationalTest {
 
 	private static void createRationals() {
 		Random random = new Random();
-		
 		/* Generate 10 random numerators and denominators in the range 0..50.
 		 * Values have a 50% chance of being negative.
 		 */
@@ -164,48 +143,4 @@ public class RationalTest {
 			else denominators.add(random.nextInt(50));
 		}
 	}
-
-	private static void displaySomeConvertsions(ArrayList<Rational> rationals, int i) {
-		String convert = "The Rational String: " + rationals.get(i).toString() + "INT: " + rationals.get(i).intValue()
-				+ "   LONG: " + rationals.get(i).longValue() + "   FLOAT: " + rationals.get(i).floatValue() 
-				+ "   DOUBLE: " + rationals.get(i).doubleValue();
-		System.out.println(convert);
-		System.out.printf("%15s%n", "The Rational String: " + rationals.get(i).toString(), "Denominator", "Rational");
-		/*System.out.println("Rational: " + rationals.get(i).toString());
-		System.out.println("int: " + rationals.get(i).intValue());
-		System.out.println("long: " + rationals.get(i).longValue());
-		System.out.println("float: " + rationals.get(i).floatValue());
-		System.out.println("double: " + rationals.get(i).doubleValue());
-	*/}
-
-	private static void displayDefault() {
-		Rational defaultRational = new Rational();
-		System.out.println("Default Constructor:\n");
-		System.out.printf("%15s%15s%15s%n", "Numerator", "Denominator", "Rational");
-		System.out.printf("%15d", defaultRational.getNumerator());
-		System.out.printf("%15d", defaultRational.getDenominator());
-		System.out.printf("%15s%n", defaultRational.toString());
-		System.out.println("\n-----------------------------------------------------------\n");
-	}
-	
-	private static void displayDenomAsOne() {
-		Random random = new Random();
-		
-		/* Generate 10 random numerators with denominator as 1 in the range 0..50. */
-		for(int i = 0; i < 10; i++) {
-			rationalsDenomOne.add(new Rational((long) random.nextInt(50)));
-		}
-		
-		System.out.println("\nDenominator As One:\n");
-		System.out.printf("%15s%15s%15s%n", "Numerator", "Denominator", "Rational");
-		
-		for(int i = 0; i < 10; i++) {
-			System.out.printf("%15d", rationalsDenomOne.get(i).getNumerator());
-			System.out.printf("%15d", rationalsDenomOne.get(i).getDenominator());
-			System.out.printf("%15s%n", rationalsDenomOne.get(i).toString());
-		}
-		System.out.println("\n-----------------------------------------------------------\n");
-
-	}
-
 }
